17dece98c9f138e2923fb5c488767e6d852edfcf
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2018-02-20 20:46:27 +0200

    Reading config.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,85 @@
-console.log("asdf");
+import * as deepMerge from "deepmerge";
+import * as fs from "fs";
+import * as yargs from "yargs";
+
+interface Config {
+    key: string;
+    secret: string;
+    databaseUrl: string;
+}
+
+const DefaultConfigObject: Config = {
+    databaseUrl: "postgres://localhost/binance-bot",
+    key: "",
+    secret: "",
+};
+
+function run(): void {
+    const args = yargs.usage("Binance-bot server. Usage: $0 [-c <config file>]")
+        .options("config", {
+            alias: "c",
+            default: "./config.json",
+            describe: "Read setting from specified config file path",
+            type: "string",
+        })
+        .locale("en")
+        .version()
+        .help("help")
+        .epilog("Support: https://github.com/Taisiias/binance-bot")
+        .strict()
+        .argv;
+    const config = readConfig(args.config as string);
+    console.log(`DatabaseUrl: ${config.databaseUrl}`);
+}
+
+function readConfig(path: string): Config {
+    if (!fs.existsSync(path)) {
+        throw new Error(`Config file was not found.`);
+    }
+
+    let fileContent = "";
+    try {
+        fileContent = fs.readFileSync(path).toString();
+    } catch (e) {
+        throw new Error(`Config file cannot be read. ${e}`);
+    }
+    let cf: Config;
+    try {
+        if (fileContent === "") {
+            throw new Error(`Config file is empty`);
+        }
+        cf = createConfigObject(fileContent);
+    } catch (e) {
+        throw new Error(`Config file cannot be parsed. ${e}`);
+    }
+    return cf;
+}
+
+function createConfigObject(fileContent: string): Config {
+    let cf: Config;
+    const json = JSON.parse(fileContent) as Config;
+    /* tslint:disable:no-any */
+    const mergedObject: { [k: string]: any } = deepMerge(DefaultConfigObject, json);
+
+    if (!mergedObject.hasOwnProperty("key") && !mergedObject.key) {
+        throw new Error(`Property key is missing.`);
+    }
+
+    if (!mergedObject.hasOwnProperty("secret") && !mergedObject.secret) {
+        throw new Error(`Property secret is missing.`);
+    }
+
+    cf = mergedObject as Config;
+
+    return cf;
+}
+
+function runAndReport(): void {
+    try {
+        run();
+    } catch (e) {
+        console.log(`An error occurred: ${e.message}`);
+    }
+}
+
+runAndReport();
