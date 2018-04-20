251d8eb98b74a66497462599aa772ae45e606db1
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2018-04-20 21:17:44 +0300

    Preparing chart.

---
+++ analytics/index.ts
@@ -0,0 +1,12 @@
+// import * as message from "./message";
+
+function insertText(): void {
+    const elem = document.getElementById("response");
+    if (elem !== null) {
+        elem.innerText = "lqwrrrrrrt";
+    }
+}
+
+window.onload = () => {
+    insertText();
+};
