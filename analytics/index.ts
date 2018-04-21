fde0e957af6a6ea23a3114df95e65b29db2e64fa
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2018-04-21 21:08:19 +0300

    Adding highcharts.

--- analytics/index.ts
+++ analytics/index.ts
@@ -1,12 +1,32 @@
 // import * as message from "./message";
+import * as highcharts from "highcharts";
 
-function insertText(): void {
-    const elem = document.getElementById("response");
-    if (elem !== null) {
-        elem.innerText = "lqwrrrrrrt";
-    }
+function insertChart(): void {
+    highcharts.chart("container", {
+        chart: {
+            type: "bar",
+        },
+        title: {
+            text: "Fruit Consumption",
+        },
+        xAxis: {
+            categories: ["Apples", "Bananas", "Oranges"],
+        },
+        yAxis: {
+            title: {
+                text: "Fruit eaten",
+            },
+        },
+        series: [{
+            name: "Jane",
+            data: [1, 0, 4],
+        }, {
+            name: "John",
+            data: [5, 7, 3],
+        }],
+    });
 }
 
 window.onload = () => {
-    insertText();
+    insertChart();
 };
