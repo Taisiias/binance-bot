9b5ab958e5bda4997b4b59a5ae8cdb2be6f06a0f
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2018-04-27 23:34:22 +0300

    Preparing chart with candlesticks.

--- analytics/index.ts
+++ analytics/index.ts
@@ -1,7 +1,8 @@
 // import * as message from "./message";
+import * as fs from "fs";
 // tslint:disable-next-line:no-submodule-imports
 import * as highcharts from "highcharts/highstock";
-// import { CurrencyCandlestickRecord } from "../src/lib";
+import { CurrencyCandlestickRecord } from "../src/lib";
 
 function insertChart(): void {
     highcharts.stockChart("container", {
@@ -10,19 +11,19 @@ function insertChart(): void {
         },
 
         title: {
-            text: "AAPL Stock Price",
+            text: "Binance Bot Candlesticks",
         },
 
         series: [{
             type: "candlestick",
-            name: "AAPL Stock Price",
+            name: "Binance Bot Candlesticks",
             data: [{
                 x: 1,
                 open: 9,
                 high: 10,
                 low: 4,
                 close: 6,
-                name: "Point2",
+                name: "Point1",
                 color: "#00FF00",
             }, {
                 x: 2,
@@ -30,7 +31,7 @@ function insertChart(): void {
                 high: 10,
                 low: 7,
                 close: 7,
-                name: "Point1",
+                name: "Point2",
                 color: "#FF00FF",
             },
             {
@@ -50,11 +51,33 @@ function insertChart(): void {
     });
 }
 
-// function readData(): void {
+function readData(): void {
+    const fileContent =
+        JSON.parse(fs.readFileSync("/candlesticks/candlesticks-ADABTC.json").toString());
+    const candlesticksArray = fileContent as CurrencyCandlestickRecord[];
 
-// }
+    let i = 1;
+    const chartData: Highcharts.DataPoint[] = [];
+    for (const o of candlesticksArray) {
+        const oo: Highcharts.DataPoint = {
+            x: i,
+            open: Number.parseFloat(o.open),
+            high: Number.parseFloat(o.high),
+            low: Number.parseFloat(o.low),
+            close: Number.parseFloat(o.close),
+            name: `Candlestick ${i}`,
+            color: "#FF00FF",
+        };
+        chartData.push(oo);
+        // const oo: Highcharts.DataPoint  = o;
+        // oo.name = `Candlestick ${i}`;
+        // oo.color = "#FF00FF";
+        i++;
+    }
+
+}
 
 window.onload = () => {
-    // readData();
+    readData();
     insertChart();
 };
