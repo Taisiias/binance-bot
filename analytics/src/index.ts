d0097e10215bb40ab55f3d990b6d3074dce51459
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2018-05-29 22:02:13 +0300

    Playing with d3.

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -1,5 +1,6 @@
-import chartjs from "chart.js";
+// import chartjs from "chart.js";
 import "chartjs-plugin-zoom";
+import * as d3 from "d3";
 import { analyzeCurrency, ReportFormat } from "./analyze";
 import { CurrencyCandlestickRecord } from "./lib";
 
@@ -9,86 +10,6 @@ function insertChart(
     // const [averageData, redCandleSticksData, greenCandleSticksData] =
     //     analyzeCurrency(candlesticksArray, 240, 2, ReportFormat.MASell, -3);
     // console.log(averageData[0]);
-
-    // highcharts.stockChart("container", {
-    //     rangeSelector: {
-    //         selected: 0,
-    //     },
-    //     yAxis: {
-    //         scrollbar: {
-    //             enabled: true,
-    //         },
-    //     },
-    //     chart: {
-    //         zoomType: "y",
-    //         panning: true,
-    //         resetZoomButton: {
-    //             position: {
-    //                 x: 0,
-    //                 y: -70,
-    //             },
-    //         },
-    //     },
-    //     title: {
-    //         text: "Binance Bot Candlesticks",
-    //     },
-    //     plotOptions: {
-    //         series: {
-    //             turboThreshold: 0,
-    //         },
-    //         line: {
-    //             lineWidth: 1,
-    //             marker: {
-    //                 radius: 1,
-    //             },
-    //         },
-    //         spline: {
-    //             lineWidth: 0,
-    //             marker: {
-    //                 enabled: true,
-    //                 radius: 2,
-    //             },
-    //         },
-    //     },
-    //     series: [
-    //         {
-    //             type: "spline",
-    //             name: "Red Binance Bot Candlesticks",
-    //             data: redCandleSticksData,
-    //             color: "red",
-    //         },
-    //         {
-    //             type: "spline",
-    //             name: "Green Binance Bot Candlesticks",
-    //             data: greenCandleSticksData,
-    //             color: "green",
-    //         },
-    //         {
-    //             type: "line",
-    //             name: "Binance Bot Average 60",
-    //             data: averageData[0],
-    //             color: "magenta",
-    //         },
-    //         {
-    //             type: "line",
-    //             name: "Binance Bot Average 240",
-    //             data: averageData[1],
-    //             color: "blue",
-    //         },
-    //         {
-    //             type: "line",
-    //             name: "Binance Bot Average 720",
-    //             data: averageData[2],
-    //             color: "black",
-    //         },
-    //         {
-    //             type: "line",
-    //             name: "Binance Bot Average 1440",
-    //             data: averageData[3],
-    //             color: "brown",
-    //         },
-    //     ],
-    // });
 }
 
 export function readData(symbol: string): CurrencyCandlestickRecord[] {
@@ -105,54 +26,17 @@ window.onload = () => {
         analyzeCurrency(candlesticksArray, 240, 2, ReportFormat.MASell, -3);
     console.log(averageData[0]);
     console.log(candleSticksData[0]);
+    d3.select(".myclass")
+        .attr("margin", "top: 20, right: 20, bottom: 30, left: 50")
+        .attr("style", "color: red");
 
-    const chart = new chartjs.Chart("myChart", {
-        type: "scatter",
-        data: {
-            // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "White", "Beige"],
-            datasets: [{
-                label: "Binance Bot CandleSticks and Moving Averages",
-                data: candleSticksData, // [12, 19, 3, 5, 2, 3, 10, 7],
-                fill: false,
-                type: "line",
-                borderColor: "red",
-                borderWidth: 1,
-                backgroundColor: "green",
-            }],
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                }],
-            },
+    d3.select("svg").append("line")
+        .attr("x1", 100)
+        .attr("y1", 100)
+        .attr("x2", 200)
+        .attr("y2", 200)
+        .style("stroke", "rgb(255,255,0)")
+        .style("stroke-width", 2);
 
-            // Container for pan options
-            pan: {
-                // Boolean to enable panning
-                enabled: true,
-
-                // Panning directions. Remove the appropriate direction to disable
-                // Eg. 'y' would only allow panning in the y direction
-                mode: "xy",
-            },
-
-            // Container for zoom options
-            zoom: {
-                // Boolean to enable zooming
-                enabled: true,
-                // drag: true,
-
-                // Zooming directions. Remove the appropriate direction to disable
-                // Eg. 'y' would only allow zooming in the y direction
-                mode: "xy",
-            },
-            // tslint:disable-next-line:no-any
-        } as any,
-    });
-    // chart.update();
-    console.log(chart.ctx);
     insertChart();
 };
